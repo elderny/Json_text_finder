db8af769f10c3e3850b66d265d9f4d117d01d429
Elderny <84969656+elderny@users.noreply.github.com>
2021-08-01 21:03:52 -0700

    Update js_5.js

--- js_5.js
+++ js_5.js
@@ -8,6 +8,7 @@ chkBtn.addEventListener('click', chkBtnfunctionOld);
 let chkTxt = document.getElementById('chkTxt');
 let responseTxt = document.getElementById('responseTxt');
 
+//We will make some animation of checking button that will run for 2 secs before showing us results
 function chkBtnfunctionOld() {
     responseTxt.innerText = 'Checking';
     responseTxt.style.backgroundColor = `rgb(255, 166, 0)`;
@@ -37,10 +38,14 @@ function chkBtnfunction() {
             let str = [];
             let results = document.getElementById('results');
             var index = 0;
+            
+            //we will push the description value into our str array
             for (key in obj) {
                 str.push(`<li id="${index++}">${obj[key].description}</li>`);
             }
             let Txt_includes = String(str.includes(chkTxt.value));
+
+            //Don't worry about Txt_noIncludes code i got it from stackoverflow its not much it works as a not included function
             let Txt_noIncludes = str.filter(item => !str.includes(item));
             if (chkTxt.value === "") {
                 results.innerHTML = "";
@@ -50,19 +55,26 @@ function chkBtnfunction() {
             } else {
                 if (Txt_includes) {
                     for (let i = 0; i < index; i++) {
+                        //We will check where in the array is our inserted value
                         if (str[i].includes(chkTxt.value)) {
+                            
+                            //This will help you get other values related to the keyword also otherwise you will repeatedly get the same value again and again
                             if (results.innerHTML.includes(str[i])) {
                             } else {
+                                //We will add the str[i] to the HTML so that we can have more then 1 values in our result section
                                 results.innerHTML += str[i];
                                 responseTxt.innerText = 'Found!';
                                 responseTxt.style.backgroundColor = 'green';
                                 responseTxt.style.color = 'white';
                                 responseTxt.style.padding = 'padding: 17px 0px;';
+                                //We will return as soon as our value is found so that we don't waste time
                                 return;
                             }
                         }
                     }
                 }
+                //I couldn't use the !Txt_includes so i needed to get a different true value code
+                //else if won't work on this
                 if (Txt_noIncludes) {
                     results.innerHTML = "";
                     responseTxt.style.color = 'white';
